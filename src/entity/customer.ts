82da07bf34b5a6cba4525d4914f279831d5b69e5
salesof7 <jpsaless2002@gmail.com>
2023-05-18 15:31:34 -0300

    feat: create order service

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -5,6 +5,7 @@ export class Customer {
   _name: string;
   _address!: Address;
   _active: boolean = false;
+  private _rewardPoints: number = 0;
 
   constructor(id: string, name: string) {
     this._id = id;
@@ -21,8 +22,16 @@ export class Customer {
     }
   }
 
+  get id(): string {
+    return this._id;
+  }
+
   get name(): string {
     return this._name;
+  }
+
+  get rewardPoints(): number {
+    return this._rewardPoints;
   }
 
   isActive(): boolean {
@@ -46,5 +55,9 @@ export class Customer {
 
   deactivate(): void {
     this._active = false;
+  }
+
+  addRewardPoints(points: number): void {
+    this._rewardPoints += points;
   }
 }
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -4,8 +4,6 @@ import { type Product } from "../entity/product";
 export default class ProductService {
   static increasePrice(products: Product[], percentage: number): void {
     products.forEach((product) => {
-      console.log(product.price * (percentage / 100));
-      console.log(product.price);
       product.changePrice(product.price + product.price * (percentage / 100));
     });
   }
