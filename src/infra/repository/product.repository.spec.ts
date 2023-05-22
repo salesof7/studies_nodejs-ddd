b08b35fb33482a4f24c7a38d01e39f5b8d2372c6
salesof7 <jpsaless2002@gmail.com>
2023-05-22 19:14:11 -0300

    feat: ensure create a product

--- src/infra/repository/product.repository.spec.ts
+++ src/infra/repository/product.repository.spec.ts
@@ -1,4 +1,7 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
+import ProductRepository from "./product.repository";
+import { Product } from "../../domain/entity/product";
+import { ProductModel } from "../db/sequelize/model/product.model";
 
 describe("Product repository test", () => {
   let sequelize: Sequelize;
@@ -10,9 +13,23 @@ describe("Product repository test", () => {
       logging: false,
       sync: { force: true },
     });
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
   });
 
   afterEach(async () => {
     await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+    await productRepository.create(product);
+    const productModel = await ProductModel.findOne({ where: { id: "1" } });
+    expect(productModel.toJSON()).toStrictEqual({
+      id: "1",
+      name: "Product 1",
+      price: 100,
+    });
   });
 });
