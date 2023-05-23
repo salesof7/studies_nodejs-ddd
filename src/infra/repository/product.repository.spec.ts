3961b9bcce7118e946d1edfa2289375e3b5fb6c3
salesof7 <jpsaless2002@gmail.com>
2023-05-23 17:24:20 -0300

    feat: implement ProductRepository

--- src/infra/repository/product.repository.spec.ts
+++ src/infra/repository/product.repository.spec.ts
@@ -32,4 +32,46 @@ describe("Product repository test", () => {
       price: 100,
     });
   });
+
+  it("should update a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("2", "Product 2", 100);
+    await productRepository.create(product);
+    product.changeName("Product 3");
+    product.changePrice(300);
+    await productRepository.update(product);
+    const productModel = await ProductModel.findOne({ where: { id: "2" } });
+    expect(productModel.toJSON()).toStrictEqual({
+      id: "2",
+      name: "Product 3",
+      price: 300,
+    });
+  });
+
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+    await productRepository.create(product);
+    const productModel = await ProductModel.findOne({ where: { id: "1" } });
+    const foundProduct = await productRepository.find("1");
+    expect(productModel.toJSON()).toStrictEqual({
+      id: foundProduct.id,
+      name: foundProduct.name,
+      price: foundProduct.price,
+    });
+  });
+
+  it("should find all products", async () => {
+    const productRepository = new ProductRepository();
+    const product1 = new Product("1", "Product 1", 100);
+    await productRepository.create(product1);
+
+    const product2 = new Product("2", "Product 2", 100);
+    await productRepository.create(product2);
+
+    const foundProducts = await productRepository.findAll();
+    const products = [product1, product2];
+
+    expect(foundProducts).toEqual(products);
+  });
 });
--- src/infra/repository/product.repository.ts
+++ src/infra/repository/product.repository.ts
@@ -1,4 +1,4 @@
-import { type Product } from "../../domain/entity/product";
+import { Product } from "../../domain/entity/product";
 import { type ProductRepositoryInterface } from "../../domain/repository/product-repository.interface";
 import { ProductModel } from "../db/sequelize/model/product.model";
 
@@ -12,14 +12,29 @@ export default class ProductRepository implements ProductRepositoryInterface {
   }
 
   async update(entity: Product): Promise<void> {
-    throw new Error("Method not implemented.");
+    await ProductModel.update(
+      {
+        name: entity.name,
+        price: entity.price,
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      }
+    );
   }
 
   async find(id: string): Promise<Product> {
-    throw new Error("Method not implemented.");
+    const productModel = await ProductModel.findOne({ where: { id } });
+    return new Product(productModel.id, productModel.name, productModel.price);
   }
 
   async findAll(): Promise<Product[]> {
-    throw new Error("Method not implemented.");
+    const productModels = await ProductModel.findAll();
+    return productModels.map(
+      (productModel) =>
+        new Product(productModel.id, productModel.name, productModel.price)
+    );
   }
 }
