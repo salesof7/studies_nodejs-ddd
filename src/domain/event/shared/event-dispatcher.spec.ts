84d6930577bae903e2e32f1e7a9094068bc06ead
salesof7 <jpsaless2002@gmail.com>
2023-05-26 10:03:57 -0300

    feat: ensure notification when notify function is called

--- src/domain/event/shared/event-dispatcher.spec.ts
+++ src/domain/event/shared/event-dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import { SendEmailWhenProductIsCreatedHandler } from "../product/handler/send-email-when-product-is-created.handler";
+import { ProductCreatedEvent } from "../product/product-created.event";
 import { EventDispatcher } from "./event-dispatcher";
 
 describe("Domain events tests", () => {
@@ -46,5 +47,26 @@ describe("Domain events tests", () => {
     expect(eventDispatcher.getEventHandlers.ProductCreatedEvent).toBe(
       undefined
     );
+  });
+
+  it("should notify all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers.ProductCreatedEvent.at(0)
+    ).toMatchObject(eventHandler);
+
+    const productCreatedEvent = new ProductCreatedEvent({
+      name: "Product 1",
+      description: "Product description",
+      price: 10,
+    });
+
+    eventDispatcher.notify(productCreatedEvent);
+    expect(spyEventHandler).toHaveBeenCalled();
   });
 });
--- src/domain/event/shared/event-dispatcher.ts
+++ src/domain/event/shared/event-dispatcher.ts
@@ -36,6 +36,11 @@ export class EventDispatcher implements EventDispatcherInterface {
   }
 
   notify(event: EventInterface): void {
-    throw new Error("Method not implemented.");
+    const eventName = event.constructor.name;
+    if (this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName].forEach((eventHandler) => {
+        eventHandler.handle(event);
+      });
+    }
   }
 }
