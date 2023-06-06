6a66c79ceaf133d97c7df577ba09422100275627
salesof7 <jpsaless2002@gmail.com>
2023-06-05 21:59:32 -0300

    test: ensure throw error if not find a customer in find customer use case

--- src/usecases/customer/find/find.customer.usecase.spec.ts
+++ src/usecases/customer/find/find.customer.usecase.spec.ts
@@ -62,4 +62,20 @@ describe("Unit Test find customer use case", () => {
     const result = await usecase.execute(input);
     expect(result).toEqual(output);
   });
+
+  it("should not find a customer", async () => {
+    const customerRepository = MockRepository();
+    customerRepository.find.mockImplementationOnce(() => {
+      throw new Error("Customer not found");
+    });
+    const usecase = new FindCustomerUseCase(customerRepository);
+
+    const input = {
+      id: "123",
+    };
+
+    await expect(async () => {
+      await usecase.execute(input);
+    }).rejects.toThrow("Customer not found");
+  });
 });
